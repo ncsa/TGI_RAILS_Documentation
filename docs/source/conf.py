eb60ca761227342da160be0fa3d8299cdb0f9014
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2025-03-03 08:55:20 -0600

    Update conf.py added RTD recommended config file additions
    this should hopefully fix the build error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,19 @@ author = 'NCSA'
 release = '0.1'
 version = '0.1.0'
 
+# RTD recommended config file additions
+
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # Restoring GitHub link
 
 html_context = {
